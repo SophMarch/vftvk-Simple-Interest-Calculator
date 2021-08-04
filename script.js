af1d11ac925685d099c67d7031eaf0beff19c704
Sophie Marchand <sophie.marchand@tuta.io>
2021-08-04 11:31:20 +0200

    correct css and js for focus

--- script.js
+++ script.js
@@ -13,9 +13,9 @@ function compute()
     var year = new Date().getFullYear() + parseInt(years);
     
     if(principal<=0){
-    	alert("Enter a positive number");
-      principal.focus()
-      document.getElementById("result").innerHTML="\<br\>Please give a positive amount";
+        alert("Enter a positive number");
+        principal.focus();
+        document.getElementById("result").innerHTML="\<br\>Please give a positive amount";
     } else {
     	document.getElementById("result").innerHTML="\<br\>If you deposit <mark>"+principal+"</mark>,\<br\>at an interest rate of <mark>"+rate+"%</mark>\<br\>You will receive an amount of <mark>"+interest+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>";
     }
