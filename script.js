3be1cb3c1aed11b57ecd00cc53bf27fe25cb4d13
Sophie Marchand <sophie.marchand@tuta.io>
2021-08-04 11:10:25 +0200

    update html, css, js for layout and function

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
+function updateRate()
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval +"%";
+}
+
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var principal = document.getElementById("principal").value;
+   	var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate / 100;
+    var year = new Date().getFullYear() + parseInt(years);
     
+    if(principal<=0){
+    	alert("Enter a positive number");
+      principal.focus()
+      document.getElementById("result").innerHTML="\<br\>Please give a positive amount";
+    } else {
+    	document.getElementById("result").innerHTML="\<br\>If you deposit <mark>"+principal+"</mark>,\<br\>at an interest rate of <mark>"+rate+"%</mark>\<br\>You will receive an amount of <mark>"+interest+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>";
+    }
 }
-        
