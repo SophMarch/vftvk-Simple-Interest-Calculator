e81877db53929e0db3fd92d16670cef1d01b48f3
Sophie Marchand <sophie.marchand@tuta.io>
2021-08-04 12:39:45 +0200

    correct focus

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function compute() {
 
     if (principal <= 0) {
         alert("Enter a positive number");
-        principal.focus();
+        document.getElementById("principal").focus();
     } else {
         document.getElementById("result").innerHTML = "\<br\>If you deposit <mark>" + principal + "</mark>,\<br\>at an interest rate of <mark>" + rate + "%</mark>\<br\>You will receive an amount of <mark>" + interest + "</mark>,\<br\>in the year <mark>" + year + "</mark>\<br\>";
     }
